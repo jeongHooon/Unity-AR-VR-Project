8369556d194820580566821546130d6d65016b03
jeongHooon <pkekzm94@naver.com>
2020-10-25 17:57:36 +0800

    03.UI, UI와 상호작용

---
+++ Unity ARVR/Assets/02.Scripts/EyeCast.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+public class EyeCast : MonoBehaviour
+{
+    private Transform tr;
+    private Ray ray;
+    private RaycastHit hit;
+    private Animator anim;
+    private int hashIsLook;
+    public float dist = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        anim = tr.Find("Canvas/Image").GetComponent<Animator>();
+        hashIsLook = Animator.StringToHash("IsLook");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = new Ray(tr.position, tr.forward);
+
+        Debug.DrawRay(ray.origin, ray.direction * dist, Color.green);
+
+        if(Physics.Raycast(ray, out hit, dist, 1<<8 | 1 << 9))
+        {
+            MoveCtrl.isStopped = true;
+            anim.SetBool(hashIsLook, true);
+        }
+        else
+        {
+            MoveCtrl.isStopped = false;
+            anim.SetBool(hashIsLook, false);
+        }
+    }
+}
